06f66a660f42caba1113f49464eddeb7ce8db27e
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-20 14:56:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -10,11 +10,11 @@ class GasStation
 
   def initialize(brand, unleaded_price)
     @brand = brand
-    @unleaded_price = 100
+    @unleaded_price = unleaded_pric
 
 end
 end
 
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
-dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
+dinobones = GasStation.new("Dino Bones Gas and Grill", 100)
