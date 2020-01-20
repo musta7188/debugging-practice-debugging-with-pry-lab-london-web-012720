7c78d7da5588eeb32e8c37695d26808848ea80e6
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-20 15:25:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 def start_game(player1, player2)
 
-  "Hello #{player1} & #{playar2}"
+puts "Hello #{player1} & #{playar2}"
 
 end
 
 def play_game(player2)
 
   "#{player1} is better than #{player2}"
-  binding.pry
+
 end
