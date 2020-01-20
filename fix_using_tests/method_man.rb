3ee96191f1742c5e73f5a9bfcaf3c9c289a471ff
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-20 15:28:33 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -2,13 +2,13 @@ require 'pry'
 
 def start_game(player1, player2)
 
-greeting =  `Hello #{player1} & #{playar2}`
+greeting =  "Hello #{player1} & #{playar2}"
 
 greeting
 
 end
 
-def play_game(start_game(player1, player2))
+def play_game(player2)
 
   "#{player1} is better than #{player2}"
 
